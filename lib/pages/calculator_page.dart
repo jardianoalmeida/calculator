ba69cec80c7cad348e7b119f4f7a072f1bee3887
jardianoalmeida <jardiano.almeida@gmail.com>
2022-07-11 13:53:41 -0300

    finish splash_page

---
+++ lib/pages/calculator_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CalculatorPage extends StatefulWidget {
+  const CalculatorPage({Key? key}) : super(key: key);
+
+  @override
+  State<CalculatorPage> createState() => _CalculatorPageState();
+}
+
+class _CalculatorPageState extends State<CalculatorPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
+
+
+/* 
+git status
+git add .
+git status 
+git commit -m "Mensagem opcional"
+git push
+*/
--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import 'calculator_page.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
@@ -9,9 +12,29 @@ class SplashPage extends StatefulWidget {
 
 class _SplashPageState extends State<SplashPage> {
   @override
+  void initState() {
+    Future.delayed(
+      const Duration(seconds: 3),
+      () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const CalculatorPage()),
+        );
+      },
+    );
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      
+    return Scaffold(
+      body: Center(
+        child: SvgPicture.network(
+          'https://usemobile.com.br/wp-content/uploads/2020/07/logo-horizontal-site-branco.svg',
+          color: Colors.white,
+          width: MediaQuery.of(context).size.width * .5,
+        ),
+      ),
     );
   }
 }
